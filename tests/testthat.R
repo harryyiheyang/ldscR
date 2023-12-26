3e8092de2dec03222f7d141e1d8594845399d079
Harry-Yihe-Yang <harry.y.yang@foxmail.com>
2023-12-26 17:35:58 -0500

    Improve Efficiency using data.table

--- tests/testthat.R
+++ tests/testthat.R
@@ -15,24 +15,25 @@ dest_file <- "GWAS_List.rds"
 download.file(url, destfile = dest_file, mode = "wb")
 gwas_list <- readRDS(dest_file)
 file.remove(dest_file)
+
 GWAS_AFR=gwas_list[c(1,5,9,13)]
 GWAS_EAS=gwas_list[c(1,5,9,13)+1]
 GWAS_EUR=gwas_list[c(1,5,9,13)+2]
 GWAS_SAS=gwas_list[c(1,5,9,13)+3]
+
 data("AFRLDSC")
 data("EASLDSC")
 data("EURLDSC")
 data("SASLDSC")
 data("hapmap3")
+
 GWAS_AFR=filterss(gwas_data_list=GWAS_AFR,ref_panel=hapmap3)
 GWAS_SAS=filterss(gwas_data_list=GWAS_SAS,ref_panel=hapmap3)
 GWAS_EUR=filterss(gwas_data_list=GWAS_EUR,ref_panel=hapmap3)
 GWAS_EAS=filterss(gwas_data_list=GWAS_EAS,ref_panel=hapmap3)
-fitAFR1=ldsc_poet(GWAS_List=GWAS_AFR,LDSC=AFRLDSC)
-fitAFR2=ldsc_sample(GWAS_List=GWAS_AFR,LDSC=AFRLDSC)
-fitEAS1=ldsc_poet(GWAS_List=GWAS_EAS,LDSC=EASLDSC)
-fitEAS2=ldsc_sample(GWAS_List=GWAS_EAS,LDSC=EASLDSC)
-fitEUR1=ldsc_poet(GWAS_List=GWAS_EUR,LDSC=EURLDSC)
-fitEUR2=ldsc_sample(GWAS_List=GWAS_EUR,LDSC=EURLDSC)
-fitSAS1=ldsc_poet(GWAS_List=GWAS_SAS,LDSC=SASLDSC)
-fitSAS2=ldsc_sample(GWAS_List=GWAS_SAS,LDSC=SASLDSC)
+
+fitAFR1=ldscR(GWAS_List=GWAS_AFR,LDSC=AFRLDSC)
+fitEAS1=ldscR(GWAS_List=GWAS_EAS,LDSC=EASLDSC)
+fitEUR1=ldscR(GWAS_List=GWAS_EUR,LDSC=EURLDSC)
+fitSAS1=ldscR(GWAS_List=GWAS_SAS,LDSC=SASLDSC)
+
